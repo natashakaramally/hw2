15272b35480fee43bbc08a85ea443f85a8a1c3fb
Natasha Karamally <n-karamally2018@nlaw.northwestern.edu>
2018-02-06 16:56:30 -0600

    Update

---
+++ app/models/happiest_cities.rb
@@ -0,0 +1,2 @@
+class HappiestCities < ApplicationRecord
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  resources :cities
+
+  resources :happiest_cities
 
 end
